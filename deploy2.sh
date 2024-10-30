fd6678ac6faa610c9d38522b80c3fc2f637fc084
JWS <topsailor@gmail.com>
2024-10-30 13:56:35 +0900

    clarified file directories

--- deploy2.sh
+++ deploy2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # root folder 
-sudo cp download.php index.php /usr/share/hanflix/
-sudo mv /usr/share/hanflix/indexv2.php index.php
+sudo cp download.php indexv2.php /usr/share/hanflix/
+sudo mv /usr/share/hanflix/indexv2.php /usr/share/hanflix/index.php
 
 sudo chown -R nginx:nginx /usr/share/hanflix
 sudo chmod -R 755 /usr/share/hanflix
